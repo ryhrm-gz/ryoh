f80bd5f7b4d05ab1b4d26322daaed49e4ff1abaf
ryhrm-gz <gyozaoisii@icloud.com>
2022-05-30 00:31:44 +0900

    fix

--- src/hooks/useWorks.ts
+++ src/hooks/useWorks.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "react-query";
-import { fetchMicroCms } from "../utils/microCms";
+import { fetchMicroCms } from "../utils/microcms";
 
 export const fetchWorksUrl = "works?fields=id%2Ctitle%2Creleased_at%2Crole";
 
